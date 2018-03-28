f3f675aab7f05a631f37821151c5fa72a2cecc11
glyn <liu.guo@husky.neu.edu>
2018-03-27 22:00:56 -0400

    finished basic

---
+++ src/main/java/fun/glyn/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package fun.glyn.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "", "/index"})
+    public String getIndexPage() {
+        System.out.println("Hello..123...");
+        return "index";
+    }
+}
